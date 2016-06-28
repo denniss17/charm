a6b1d65703b55331d8231cb0221933bf9490a6e9
Dennis Schroer <dev@dennisschroer.nl>
2016-06-28 13:16:17 +0200

    Create binary user tree for user secrets

---
+++ charm/test/toolbox/usertree_test.py
@@ -0,0 +1,30 @@
+from charm.toolbox.usertree import BinaryUserTree
+import unittest
+
+
+class UserTreeTest(unittest.TestCase):
+    def setUp(self):
+        self.x = 0
+
+    def generator(self):
+        self.x += 1
+        return self.x
+
+    def test_get_path(self):
+        tree = BinaryUserTree(4, lambda: self.generator())
+        # This creates the following tree:
+        #           1
+        #        /     \
+        #     2            9
+        #    / \          / \
+        #  3     6     10    13
+        #  /\    /\    /\    /\
+        # 4  5  7  8  11 12 14 15
+        self.assertEqual(map(lambda x: x.value, tree.get_path(0)), [1, 2, 3, 4])
+        self.assertEqual(map(lambda x: x.value, tree.get_path(2)), [1, 2, 6, 7])
+        self.assertEqual(map(lambda x: x.value, tree.get_path(5)), [1, 9, 10, 12])
+        self.assertEqual(map(lambda x: x.value, tree.get_path(7)), [1, 9, 13, 15])
+
+
+if __name__ == "__main__":
+    unittest.main()
--- charm/toolbox/usertree.py
+++ charm/toolbox/usertree.py
@@ -1,8 +1,10 @@
+from charm.toolbox.node import BinaryNode
 
 
-class BinaryUserTreeNode:
+class BinaryUserTreeNode(BinaryNode):
     def __init__(self, value, left=None, right=None):
-        self.value = value
+        super(BinaryUserTreeNode, self).__init__(value, left, right)
+
 
 class BinaryUserTree:
     """
@@ -10,9 +12,43 @@ class BinaryUserTree:
     This can be used for efficient key updates, where the key of a user is composed of the values on the nodes from
     root to the leaf of the user. However, the maximum number of users in the tree is limited
     by the predetermined height of the tree.
-    """
-    def __init__(self, height):
-        self.height = height
 
-    def populate_nodes(self, generator):
+    The root node has level 0, the leaves have level height-1
+    """
+
+    def __init__(self, height, generator):
+        self.height = height
+        self.root = self.populate_tree(generator, height)
+
+    def populate_tree(self, generator, height):
+        """
+        Create a tree of given height and populate the nodes
+        and leaves with values returned by the generator function.
+        """
+        node = BinaryUserTreeNode(generator())
+        if height > 1:
+            node.addSubNode(self.populate_tree(generator, height - 1), self.populate_tree(generator, height - 1))
+        return node
+
+    def get_leaf(self, index):
         pass
+
+    def get_path(self, index):
+        """
+        Get the path from the root node to the leaf with given index.
+        The leftmost leaf has index 0, the rightmost leaf has index 2^(height-1)-1
+        """
+        return self._get_path(index, self.root, self.height)
+
+    def _get_path(self, index, node, height):
+        if height == 1:
+            return [node]
+        else:
+            # The number of leaves, for height 4 this is 8 (2^3)
+            size = 2 ^ (height - 1)
+            if index < size / 2:
+                # Leaf is in the left side of the tree
+                return [node] + self._get_path(index, node.left, height - 1)
+            else:
+                # Leaf is in the right side of the tree
+                return [node] + self._get_path(index - (size / 2), node.right, height - 1)
