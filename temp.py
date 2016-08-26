417b7912089ca65c2bdc0c707eebbff500f232d1
Dennis Schroer <dev@dennisschroer.nl>
2016-08-26 11:56:45 +0200

    Extend temp python

--- temp.py
+++ temp.py
@@ -4,6 +4,12 @@ from charm.toolbox.pairinggroup import PairingGroup
 
 group = PairingGroup('SS512')
 
+print("pickle(group)")
+print(pickle.dumps(group))
+
+print("unpickle(pickle(group))")
+print(pickle.loads(pickle.dumps(group)))
+
 e = group.random()
 print("e")
 print(e)
